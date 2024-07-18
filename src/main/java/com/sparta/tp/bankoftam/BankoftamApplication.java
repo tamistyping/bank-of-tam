7703f03dc5d3d8725d37669119df2235f644c33d
Tam <enquiretam@gmail.com>
2024-07-18 20:13:57 +0100

    first commit

---
+++ src/main/java/com/sparta/tp/bankoftam/BankoftamApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.tp.bankoftam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankoftamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankoftamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/tp/bankoftam/BankoftamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.tp.bankoftam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankoftamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
