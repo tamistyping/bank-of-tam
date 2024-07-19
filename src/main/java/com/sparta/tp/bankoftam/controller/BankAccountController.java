a0d481c09b2c513a70b5880fbf9ba7bdf23d85d9
Tam <enquiretam@gmail.com>
2024-07-19 20:45:25 +0100

    bank controller done

--- src/main/java/com/sparta/tp/bankoftam/controller/BankAccountController.java
+++ src/main/java/com/sparta/tp/bankoftam/controller/BankAccountController.java
@@ -1,4 +1,31 @@
 package com.sparta.tp.bankoftam.controller;
 
+import com.sparta.tp.bankoftam.service.BankAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/bank-accounts")
 public class BankAccountController {
+
+    private final BankAccountService bankAccountService;
+
+    @Autowired
+    public BankAccountController(BankAccountService bankAccountService) {
+        this.bankAccountService = bankAccountService;
+    }
+
+    @PostMapping("/transfer")
+    public ResponseEntity<Void> transferMoney(
+            @RequestParam Long from,
+            @RequestParam Long to,
+            @RequestParam double amount){
+        bankAccountService.transferMoney(from, to, amount);
+        return ResponseEntity.ok().build();
+    }
 }
